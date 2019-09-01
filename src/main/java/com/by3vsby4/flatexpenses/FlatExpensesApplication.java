33d87051a3499d1815a6c973882e07bd745edf2c
Shubhanshu Ranjan <shubhanshu.ranjan@paytm.com>
2019-09-01 12:05:11 +0530

    Initial commit

---
+++ src/main/java/com/by3vsby4/flatexpenses/FlatExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.by3vsby4.flatexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/by3vsby4/flatexpenses/FlatExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by3vsby4.flatexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
